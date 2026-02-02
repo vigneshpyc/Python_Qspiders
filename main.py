a71e518118f698219814e08501fcf7f79d0694b7
vigneshpyc <mkvigneshpyc@gmail.com>
2026-02-02 10:54:06 +0530

    type casting

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello world !")
+#02.02.2026 Type casting/conversion
+a = 25
+print(a)
+print(type(a))
+float(a) # convert the a into float datatype
+complex(0,a) # convert the a into complex datatype which consiste of real and imaginary part
+complex(a,a) # 25+25j
+bool(a) # convert the a into bool datatype
+str(a) #convert the a into string datatype
+
